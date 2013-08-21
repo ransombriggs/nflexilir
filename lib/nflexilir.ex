27985bc3325858440f4cd050ad232ac9d8bcc245
Ransom Briggs <ransom-briggs@uiowa.edu>
2013-08-21 17:19:11 -0500

    Making path relative and parsing 2013 json

--- lib/nflexilir.ex
+++ lib/nflexilir.ex
@@ -1,7 +1,21 @@
 defmodule Nflexilir do
   def json_files do
-    json_dir = "/home/rbriggs/git/nflgame/nflgame/gamecenter-json.json"
+    json_dir = "lib/gamecenter-json.json"
     {:ok, files} = File.ls(json_dir)
-    files
+    Enum.filter(files, fn(x) -> String.starts_with?(x, "2013") end)
+  end
+
+  def json_content do
+    Enum.map(json_files, fn(x) ->
+        filename = "lib/gamecenter-json.json/#{x}"
+        File.read!(filename)
+    end)
+  end
+
+  def json_objects do
+    Enum.map(json_content, fn(x) ->
+        {:ok, json} = JSON.decode(x)
+        json
+    end)
   end
 end
--- mix.exs
+++ mix.exs
@@ -16,6 +16,8 @@ defmodule Nflexilir.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [
+      {:json,"0.1.0",[github: "cblage/elixir-json"]}
+    ]
   end
 end
