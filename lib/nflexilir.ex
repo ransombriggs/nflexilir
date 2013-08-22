81e2fa82a7d58c0a46c0003c8ae3745a8da9df1a
Ransom Briggs <ransom-briggs@uiowa.edu>
2013-08-22 12:34:47 -0500

    Parsing out the key based on filename

--- lib/nflexilir.ex
+++ lib/nflexilir.ex
@@ -5,17 +5,13 @@ defmodule Nflexilir do
     Enum.filter(files, fn(x) -> String.starts_with?(x, "2013") end)
   end
 
-  def json_content do
-    Enum.map(json_files, fn(x) ->
-        filename = "lib/gamecenter-json.json/#{x}"
-        File.read!(filename)
-    end)
-  end
-
   def json_objects do
-    Enum.map(json_content, fn(x) ->
-        {:ok, json} = JSON.decode(x)
-        json
+    Enum.map(json_files, fn(x) ->
+      filename = "lib/gamecenter-json.json/#{x}"
+      {:ok, json} = JSON.decode(File.read!(filename))
+
+      key = Enum.fetch!(Regex.run(%r/^([0-9]+)\.json$/, x), 1)
+      Dict.fetch!(json, key)
     end)
   end
 end
