324c09e7cf812165ca66929a4cf87bd8fff9a801
Ransom Briggs <ransom-briggs@uiowa.edu>
2013-08-28 23:44:54 -0500

    rails new draft

---
+++ draft/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Draft::Application.config.secret_key_base = '2a908597cd7221acdd4b21f1dce14681e851f0a99260666310f802dd8fd4ee8e04556b728662d89d82bcf49f7075d8c166b7e79a7b9f90438ec4f98f66c539c9'
---
+++ draft/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Draft::Application.config.session_store :cookie_store, key: '_draft_session'
