99f7fc87ea6f1d22109325b4db5f7339d4ba09f2
Ransom Briggs <ransom-briggs@uiowa.edu>
2013-08-29 21:52:28 -0500

    rails g scaffold Player

--- draft/config/routes.rb
+++ draft/config/routes.rb
@@ -1,4 +1,6 @@
 Draft::Application.routes.draw do
+  resources :players
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
